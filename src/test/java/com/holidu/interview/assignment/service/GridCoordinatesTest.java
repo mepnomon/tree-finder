560aa1bcb056300c1d4ac9471abdaad74c197353
doriandressler <dorian@data-lens.co.uk>
2019-08-31 23:56:10 +0100

    Getter tests added for GridCoordinates

---
+++ src/test/java/com/holidu/interview/assignment/service/GridCoordinatesTest.java
@@ -0,0 +1,21 @@
+package com.holidu.interview.assignment.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GridCoordinatesTest {
+
+    private GridCoordinates gridCoordinates;
+
+    @Test
+    public void test_getters_passes_if_values_match(){
+        int top = 5, bottom = 8, left = -19, right = 9;
+        gridCoordinates = new GridCoordinates(bottom, top, left, right);
+        assertEquals(gridCoordinates.getRight(), right);
+        assertEquals(gridCoordinates.getLeft(), left);
+        assertEquals(gridCoordinates.getTop(), top);
+        assertEquals(gridCoordinates.getBottom(), bottom);
+    }ß
+}
