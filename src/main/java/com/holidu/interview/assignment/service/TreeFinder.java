78831fabcee977060f509671a1a939450a337d31
doriandressler <dorian@data-lens.co.uk>
2019-09-01 16:32:11 +0100

    added conversion

--- src/main/java/com/holidu/interview/assignment/service/TreeFinder.java
+++ src/main/java/com/holidu/interview/assignment/service/TreeFinder.java
@@ -1,17 +1,22 @@
 package com.holidu.interview.assignment.service;
 
+import com.holidu.interview.assignment.util.UnitConverter;
+
 public class TreeFinder {
 
 
+
+    UnitConverter unitConverter = new UnitConverter();
+
     public GridCoordinates getTreeGrid(double x, double y, double radiusInMeters) {
 
+        double radiusInFeet = unitConverter.convertToUSFeet(radiusInMeters);
 
-        // THIS CALCULATION IS CRAP
+        double bottom = y - radiusInFeet;
+        double top = y + radiusInFeet;
+        double left = x - radiusInFeet;
+        double right = x + radiusInFeet;
 
-        double bottom = y - radiusInMeters;
-        double top = y + radiusInMeters;
-        double left = x - radiusInMeters;
-        double right = x + radiusInMeters;
         return new GridCoordinates(bottom, top, left, right);
     }
 }
--- src/test/java/com/holidu/interview/assignment/util/UnitConverterTest.java
+++ src/test/java/com/holidu/interview/assignment/util/UnitConverterTest.java
@@ -1,10 +1,9 @@
 package com.holidu.interview.assignment.util;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UnitConverterTest {
 
